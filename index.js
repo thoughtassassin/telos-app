e864e2ec1e05d2c2aea43328a78513162a748a59
David <david@onerootelement.com>
2022-05-18 22:52:50 -0500

    Fix formatting Add dynamic version of current pdf

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import fs from "fs/promises";
 import { PDFDocument } from "pdf-lib";
 
 const fetchAndParsePDF = async () => {
-  const data = await fs.readFile(
-    "1200-eng2011.pdf"
-  );  
+  const data = await fs.readFile("1200-eng2011.pdf");
   const pdfDoc = await PDFDocument.load(data);
   const form = pdfDoc.getForm();
   const fields = form.getFields();
-  fields.forEach((field, index) => console.log(`${index + 1}. ${field.getName()}`));
+  fields.forEach((field, index) =>
+    console.log(`${index + 1}. ${field.getName()}`)
+  );
 };
 
 fetchAndParsePDF();
