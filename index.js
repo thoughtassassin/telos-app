4c0fcb69628714c3ce066bcd9dd15af3b466fc82
David <david@onerootelement.com>
2022-05-28 13:44:47 -0500

    Upload pdf to S3 by API Gateway

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 const fs = require("fs/promises");
 const { PDFDocument } = require("pdf-lib");
+const AWS = require('aws-sdk');
+const path = require("path");
 
 exports.handler = async (event) => {
   const data = await fs.readFile("medicare-form.pdf");
   const pdfDoc = await PDFDocument.load(data);
   const form = pdfDoc.getForm();
-  const fields = form.getFields();
-  const fieldsList = fields.map(
-    (field, index) => `${index + 1}. ${field.getName()}`
-  );
 
-  form.getTextField("firstname").setText("Clark");
-  form.getTextField("lastname").setText("Kent");
-  form.getTextField("spouseFirstname").setText("Lois");
-  form.getTextField("spouseLastname").setText("Lane");
+  form.getTextField("firstname").setText("Green");
+  form.getTextField("lastname").setText("Arrow");
+  form.getTextField("spouseFirstname").setText("Black");
+  form.getTextField("spouseLastname").setText("Canary");
 
-  fs.writeFile("saved-medicare-form.pdf", await pdfDoc.save());
-
+  AWS.config.update({ region: "us-east-1" });
+  s3 = new AWS.S3({ apiVersion: "2006-03-01" });
+  const uploadParams = {
+    Bucket: "medicareforms3",
+    Key: path.basename("new-saved-medicare.pdf"),
+    Body: await pdfDoc.save(),
+  };
+  s3.upload(uploadParams, function (err, data) {
+    if (err) {
+      console.log("Error", err);
+    }
+    if (data) {
+      console.log("Upload Success", data.Location);
+    }
+  });
+  
   return {
     statusCode: 200,
-    body: fieldsList,
+    body: JSON.stringify({ response: 'File uploaded' }),
   };
 };
